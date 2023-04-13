23bca1f9a1df72ef685c92ccae211baf70351868
yakubalkis <alkis.yakub@gmail.com>
2023-04-13 13:20:45 +0300

    Loaded entity classes.

---
+++ src/main/java/com/luv2code/model/Role.java
@@ -0,0 +1,38 @@
+package com.luv2code.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String roleUser) {
+        this.name = roleUser;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
